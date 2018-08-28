4bdd67b3995db9dfdcf96e845c4c679397a66d2b
Unknown <kelly.olsen@progleasing.com>
2018-08-28 13:16:31 -0600

    Add nested page

---
+++ src/app/child-one/child-one.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-one',
+  templateUrl: './child-one.page.html',
+  styleUrls: ['./child-one.page.scss'],
+})
+export class ChildOnePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
